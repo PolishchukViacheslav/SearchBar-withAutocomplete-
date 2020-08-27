9d3fb79376d261b7cee5d5a0680b4927f24fe05e
Slava Polishchuk <dizerllc@gmail.com>
2020-08-27 10:48:18 +0300

    continue styling

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -1,16 +1,21 @@
 <template>
     <Suspense>
-        <div id="show-blogs">
+        <div class="header">
             <h1>All Blog Articles</h1>
-            <input type="text" v-model="blogTitle" autocomplete="off" placeholder="search blogs" />
-            <!-- <div v-for="blog in filteredBlogs" :key="blog.title" class="single-blog">
-                <h2>{{ blog.title }}</h2>
-                <article>{{ blog.body }}</article>
-            </div> -->
-            <div v-if="filteredBlogs">
-                <ul>
-                    <li v-for="blog in filteredBlogs" :key="blog.id" @click="setBlogTitle(blog.title)">{{blog.title}}</li>
-                </ul>
+            <div class="header__wrapper">
+                <input type="text" v-model="blogTitle" autocomplete="off" placeholder="type blog title here" class="header__input" />
+                <!-- <div v-for="blog in filteredBlogs" :key="blog.title" class="single-blog">
+                    <h2>{{ blog.title }}</h2>
+                    <article>{{ blog.body }}</article>
+                </div> -->
+                <div v-if="isSearchResult">
+                    <ul class="header__autoCompleteList list">
+                        <li v-for="blog in filteredBlogs" :key="blog.id" @click="setBlogTitle(blog.title)" class="list__item">
+                            <img src="../../public/searchIcon.svg" alt="search icon" class="list__icon">
+                            {{blog.title}}
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </Suspense>
@@ -22,7 +27,8 @@ export default {
         return {
             blogs: [],
             blogTitle: '',
-            filteredBlogs: []
+            filteredBlogs: [],
+            isSearchResult: false
         }
     },
     methods: {
@@ -33,22 +39,27 @@ export default {
         //     console.log("input",this.blogTitle);
         // }
         setBlogTitle(title) {
-            this.blogTitle = title
+            this.blogTitle = title;
+            this.isSearchResult = false;
+            console.log("blogs", this.filteredBlogs, "flag-set",this.isSearchResult);
+
         }
     },
     watch: {
         blogTitle() {
+            console.log('second');
             this.filteredBlogs = this.blogs
                 .filter(({ title }) => {
-                const regExp = new RegExp(`${this.blogTitle}`,'i');
-                return regExp.test(title);
+                    if (this.blogTitle) {
+                        const regExp = new RegExp(`${this.blogTitle}`,'i');
+                        return regExp.test(title)
+                    }
+                    return false;
                 })
                 .map((blog) => ({title: blog.title, id: blog.id}));
-            console.log("input",this.filteredBlogs);
-            // console.log('blogs', this.filteredBlogs.map(blog => blog.title));
-            
         }
     },
+    
     async mounted() {
         const blogsresponse = await fetch('https://jsonplaceholder.typicode.com/posts');
         this.blogs = await blogsresponse.json();
@@ -65,11 +76,75 @@ export default {
 </script>
 
 <style>
-#show-blogs{
-    max-width: 800px;
-    margin: 0px auto;
+.header {
+    display: grid;
+    place-items: center;
+    border-bottom: 1px solid #ffa07a;
+    width: 100%;
+    min-height: 100px;
+    margin-bottom: 80px;
+    background-color: #f0f8ff;
+    font-size: 26px;
 }
-.single-blog{
+
+.header__wrapper {
+    position: relative;
+    width: 560px;
+    padding-bottom: 10px;
+}
+
+.header__input {
+    padding: 10px;  
+    width: inherit;
+    border: 1px solid lightgray;
+    border-radius: 8px;
+}
+
+.header__input:focus {
+    outline: none;
+    box-shadow: 0 4px 6px 0 rgba(32,33,36,0.28);
+}
+
+.header__input::placeholder {
+    color: lightgray;
+}
+
+.header__autoCompleteList {
+    z-index: 1;
+    position: absolute;
+    margin: 0;
+    padding: 0;
+    width: inherit;
+    background-color:#f0f8ff;
+    box-shadow: 0 4px 6px 0 rgba(32,33,36,0.28);
+}
+
+.list {
+    max-height: 500px;
+    overflow: scroll;
+    list-style-type: none;
+}
+
+.list__item {
+    box-sizing: border-box;
+    border-bottom: 1px solid #cec7c7;
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    width: 560px;
+}
+
+.list__item:last-child {
+    border: none;
+}
+
+.list__icon {
+    margin-right: 10px;
+    width: 0.7em;
+    height: 0.7em;
+}
+
+.single-blog {
     padding: 20px;
     margin: 20px 0;
     box-sizing: border-box;
